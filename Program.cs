334dc158ac2f5254b47f46393a7929ea4370cb3a
Konstantin <kbynevskij@yandex.ru>
2023-11-14 21:05:17 +0100

    завершено

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿//  
+﻿
 //  Задача: Написать программу, которая из имеющегося 
 //  массива строк формирует новый массив из строк, длина которых 
 //  меньше, либо равна 3 символам. Первоначальный массив можно ввести
@@ -9,7 +9,7 @@
 //  Примеры:
 //  [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
   
-string[] array = new string[4] {“Hello”, “2”, “world”, “:-)”};
+string[] array = new string[4] {"Hello", "2", "world", ":-)"};
 
 string[] array2 = new string[array.Length];
 
@@ -19,20 +19,23 @@ void FirstArrayMoving(string[] array, string[] array2)
 
         for (int i = 0; i < array.Length; i++)
         {
-            if(array1[i].Length <= 3)
+            if(array[i].Length <= 3)        // максимальное кол-во символов в одной ячейке
                 {
-                array2[count] = array1[i];
+                array2[count] = array[i];
                 count++;
                 }
         }
 }   
 void PrintArray(string[] array)
 {
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array2.Length; i++)
     {
-        Console.Write($"{array[i]} ");
+        Console.Write($"{array2[i]} ");
+    
     }
     Console.WriteLine();
 }
-SecondArrayWithIF(array1, array2);
+FirstArrayMoving(array, array2);
 PrintArray(array2);
+
+
