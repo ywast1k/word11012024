81528add10344f8f4e2a89e9a433c328e0e1e264
Konstantin <kbynevskij@yandex.ru>
2023-11-14 20:49:29 +0100

    status

--- Program.cs
+++ Program.cs
@@ -9,7 +9,30 @@
 //  Примеры:
 //  [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
   
-string[] array1 = new string[4] {“Hello”, “2”, “world”, “:-)”};
+string[] array = new string[4] {“Hello”, “2”, “world”, “:-)”};
 
-string[] array2 = new string[array1.Length];
+string[] array2 = new string[array.Length];
 
+void FirstArrayMoving(string[] array, string[] array2)
+{
+    int count = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if(array1[i].Length <= 3)
+                {
+                array2[count] = array1[i];
+                count++;
+                }
+        }
+}   
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
